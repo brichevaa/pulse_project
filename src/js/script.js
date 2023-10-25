4ae77539bf1c79ff7ce478246571f5d81e75e8d5
Alina <alinatimanova@gmail.com>
2023-10-25 11:27:02 +0300

    добавила первые 4 секции

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+   $('.carousel__inner').slick({
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               arrows: false,
+               dots: true,
+            },
+         },
+      ],
+   });
+});

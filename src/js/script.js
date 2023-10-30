4681fa9fadfdd96b76e4bbcf75359f2ec65b8d04
Alina <alinatimanova@gmail.com>
2023-10-30 20:18:42 +0300

    добавила анимацию

--- src/js/script.js
+++ src/js/script.js
@@ -80,4 +80,20 @@ $(function () {
    validateForms('#order form');
 
    $('input[name=phone]').mask('+7 (999) 999-9999');
+
+   // Smooth scrool and pageup
+
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+         $('.pageup').fadeIn();
+      } else {
+         $('.pageup').fadeOut();
+      }
+   });
+
+   $("a[href^='#']").click(function () {
+      const _href = $(this).attr('href');
+      $('html, body').animate({ scrollTop: $(_href).offset().top + 'px' });
+      return false;
+   });
 });

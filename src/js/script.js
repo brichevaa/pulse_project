3cfe8ccbb0161bc9270242e0216282b9f106a993
Alina <alinatimanova@gmail.com>
2023-10-31 12:12:29 +0300

    добавила анимацию на отзывы

--- src/js/script.js
+++ src/js/script.js
@@ -96,4 +96,24 @@ $(function () {
       $('html, body').animate({ scrollTop: $(_href).offset().top + 'px' });
       return false;
    });
+
+   $('form').submit(function (e) {
+      e.preventDefault();
+      if (!$(this).valid()) {
+         return;
+      }
+
+      $.ajax({
+         type: 'POST',
+         url: 'mailer/smart.php',
+         data: $(this).serialize(),
+      }).done(function () {
+         $(this).find('input').value('');
+
+         $('form').trigger('reset');
+      });
+      return false;
+   });
+
+   new WOW().init();
 });
